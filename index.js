ec11204a6839eb8a4e220d7f4666c95032bbafec
Vannchhai <vannchhai.sti@gmail.com>
2026-02-07 14:09:48 +0700

    update format

--- index.js
+++ index.js
@@ -6,20 +6,20 @@ const PORT = process.env.PORT || 3000;
 app.use(express.json());
 
 app.get('/health', (req, res) => {
-    res.status(200).json({
-        status: 'ok',
-        timestamp: new Date().toISOString(),
-    });
+  res.status(200).json({
+    status: 'ok',
+    timestamp: new Date().toISOString(),
+  });
 });
 
 app.get('/test', (req, res) => {
-    res.send('Express test server is running 🚀');
+  res.send('Express test server is running 🚀');
 });
 
 app.get('/error', (req, res) => {
-    res.status(500).json({ error: 'Intentional test error' });
+  res.status(500).json({ error: 'Intentional test error' });
 });
 
 app.listen(PORT, () => {
-    console.log(`✅ Server running on http://localhost:${PORT}`);
+  console.log(`✅ Server running on http://localhost:${PORT}`);
 });
